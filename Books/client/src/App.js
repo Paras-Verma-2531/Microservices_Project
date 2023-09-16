16f0628ace3da283b71df2849bbef199dacf2726
paras verma <parasverma2531@gmail.com>
2023-09-16 14:56:05 +0530

    Navbar component built

--- Books/client/src/App.js
+++ Books/client/src/App.js
@@ -1,15 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Navbar from "./components/navbar/Navbar";
+import Admin from "./pages/admin/Admin";
 import Home from "./pages/home/Home";
-
+import ViewBooks from "./pages/viewBooks/ViewBooks";
 function App() {
   return (
     <>
-      <h1>Hello</h1>
       <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/nav" element={<Navbar />} />
+        <Route path="/" element={<Home />}>
+          <Route path="/viewBooks" element={<ViewBooks />} />
+          <Route path="/admin" element={<Admin />} />
+        </Route>
       </Routes>
     </>
   );
