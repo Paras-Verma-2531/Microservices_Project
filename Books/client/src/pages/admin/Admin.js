aa5d91e77ad694d98d55e190bf0681f872fbbb33
paras verma <parasverma2531@gmail.com>
2023-09-19 12:07:24 +0530

    Admin page css handled

--- Books/client/src/pages/admin/Admin.js
+++ Books/client/src/pages/admin/Admin.js
@@ -1,12 +1,15 @@
-import React from 'react'
-import CreateBook from '../../components/createBook/CreateBook'
-
+import React from "react";
+import "./Admin.scss";
+import CreateBook from "../../components/createBook/CreateBook";
 function Admin() {
   return (
-    <>
-    <CreateBook/>
-    </>
-  )
+    <div className="admin">
+      <div className="left-side">
+        <CreateBook />
+      </div>
+      <div className="right-side"></div>
+    </div>
+  );
 }
 
-export default Admin
+export default Admin;
